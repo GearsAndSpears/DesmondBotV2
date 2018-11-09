c5131dbbb5275a989ff0e55f5a4504f4800efbe5
thomas <thomasmholder@gmail.com>
2018-11-09 08:23:38 -0500

    Renamed Drive opmode

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP/LiftTest.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP/LiftTest.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Hardware.Accumulator;
 import org.firstinspires.ftc.teamcode.Hardware.Robot;
 
-@TeleOp(name="Lift Test", group="Tests")
+@TeleOp(name="Drive", group="Drive")
 public class LiftTest extends OpMode {
 
     Robot robot = new Robot();
