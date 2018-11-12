c82b9ca19f9aa05e094697faf4fd511fe388673e
thomas <thomasmholder@gmail.com>
2018-11-12 08:24:01 -0500

    Made encoderTest linearopmode

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP/EncoderTest.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP/EncoderTest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.TeleOP;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -7,26 +8,26 @@ import org.firstinspires.ftc.teamcode.Hardware.Accumulator;
 import org.firstinspires.ftc.teamcode.Hardware.Robot;
 
 @TeleOp(name="Encoder Test", group="Tests")
-public class EncoderTest extends OpMode {
+public class EncoderTest extends LinearOpMode {
 
     Robot robot = new Robot();
 
     @Override
-    public void init() {
+    public void runOpMode() {
 
         robot.init(hardwareMap, telemetry);
 
+        robot.setup(hardwareMap, telemetry, this);
 
         robot.lift.liftLatch.setPosition(robot.lift.liftLatchOpened);
 
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Acc Position:", robot.acc.accDrive.getCurrentPosition());
+            telemetry.addData("Lift Position:", robot.lift.liftDrive.getCurrentPosition());
+            telemetry.update();
+        }
     }
 
-    @Override
-    public void loop() {
-
-        telemetry.addData("Acc Position:", robot.acc.accDrive.getCurrentPosition());
-        telemetry.addData("Lift Position:", robot.lift.liftDrive.getCurrentPosition());
-        telemetry.update();
-
-    }
 }
