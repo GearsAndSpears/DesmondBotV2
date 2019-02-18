9b96e659b505dbb38c46c7a0edc593cfad24d7b4
thomas <thomasmholder@gmail.com>
2019-02-18 09:51:08 -0500

    Added SmartDrive

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOP/SmartDrive.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.TeleOP;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class SmartDrive extends LinearOpMode {
+
+    //Variable Declaration
+
+
+    @Override
+    public void runOpMode() {
+
+    }
+}
